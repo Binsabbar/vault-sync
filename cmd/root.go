c539dcbc6d35aeb35b0a6bbfa28b16af12050cde
Mohammed Binsabbar <ibinsabbar@gmail.com>
2025-05-18 11:14:00 +0300

    Mo/FH: cleeanup

--- cmd/root.go
+++ cmd/root.go
@@ -4,8 +4,8 @@ Copyright © 2025 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
-	"fmt"
 	"os"
+	"vault-sync/cmd/sync"
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
@@ -13,7 +13,11 @@ import (
 
 var cfgFile string
 
-var rootCmd = &cobra.Command{
+const (
+	CFG_FLAG_NAME = "config"
+)
+
+var RootCmd = &cobra.Command{
 	Use:   "vault-sync",
 	Short: "Vault Sync will sync two Vault instances together",
 	Long: `Vault Sync is a tool that will sync Vault secrets from one instance to multiple other instances.
@@ -21,35 +25,21 @@ It is useful for keeping secrets in sync across multiple regions, such as main s
 }
 
 func Execute() {
-	err := rootCmd.Execute()
+	err := RootCmd.Execute()
 	if err != nil {
 		os.Exit(1)
 	}
 }
 
 func init() {
-	cobra.OnInitialize(initConfig)
 
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.vault-sync.yaml)")
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-}
+	RootCmd.PersistentFlags().StringVarP(&cfgFile, CFG_FLAG_NAME, "c", "", "path to config file")
 
-func initConfig() {
-	if cfgFile != "" {
-		viper.SetConfigFile(cfgFile)
-	} else {
-
-		home, err := os.UserHomeDir()
-		cobra.CheckErr(err)
-
-		viper.AddConfigPath(home)
-		viper.SetConfigType("yaml")
-		viper.SetConfigName(".vault-sync")
-	}
-
+	viper.BindPFlag(CFG_FLAG_NAME, RootCmd.PersistentFlags().Lookup(CFG_FLAG_NAME))
+	viper.SetConfigName(cfgFile)
+	viper.SetConfigType("yaml")
+	viper.SetEnvPrefix("vault_sync")
 	viper.AutomaticEnv()
 
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
-	}
+	RootCmd.AddCommand(sync.SyncCmd)
 }
--- cmd/sync/sync.go
+++ cmd/sync/sync.go
@@ -1,7 +1,7 @@
 /*
 Copyright © 2025 NAME HERE <EMAIL ADDRESS>
 */
-package cmd
+package sync
 
 import (
 	"fmt"
@@ -9,17 +9,14 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// syncCmd represents the sync command
-var syncCmd = &cobra.Command{
+var SyncCmd = &cobra.Command{
 	Use:     "sync",
 	Short:   "start the sync process until it is stopped",
 	Long:    `it will start the sync process and keep it running until it is stopped`,
 	Example: `vault-sync sync --config /path/to/config.yaml`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("sync called")
-	},
+	Run:     run,
 }
 
-func init() {
-	rootCmd.AddCommand(syncCmd)
+func run(cmd *cobra.Command, args []string) {
+	fmt.Println("sync called")
 }
