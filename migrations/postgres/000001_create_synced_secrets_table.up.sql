dc7abc5467dbf164c52f0192dd7fc90368176b28
Mo <Binsabbar@users.noreply.github.com>
2025-05-21 14:13:53 +0300

    Mo: add migration script to init the schema

--- migrations/postgres/000001_create_synced_secrets_table.up.sql
+++ migrations/postgres/000001_create_synced_secrets_table.up.sql
@@ -1,12 +1,13 @@
 CREATE TABLE IF NOT EXISTS synced_secrets (
     id SERIAL PRIMARY KEY,
     secret_path TEXT NOT NULL,
+    secret_backend TEXT NOT NULL,
     source_version INTEGER NOT NULL,
     destination_cluster TEXT NOT NULL,
-    destination_version INTEGER NOT NULL,
+    destination_version INTEGER,
     last_sync_attempt TIMESTAMPTZ NOT NULL,
     last_sync_success TIMESTAMPTZ,
     status TEXT NOT NULL,
     error_message TEXT,
-    UNIQUE (secret_path, destination_cluster)
+    UNIQUE (secret_backend, secret_path, destination_cluster)
 );
