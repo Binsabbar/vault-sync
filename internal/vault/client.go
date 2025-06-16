8dfdef147671faa17368a007539fcd5fccc76470
Mohammed Binsabbar <ibinsabbar@gmail.com>
2025-06-16 12:49:40 +0300

    Mo: implement token checker and re-authenticate

--- internal/vault/client.go
+++ internal/vault/client.go
@@ -4,13 +4,16 @@ package vault
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"sync"
 	"vault-sync/internal/config"
+	"vault-sync/pkg/converter"
 	"vault-sync/pkg/log"
 
 	"github.com/hashicorp/vault-client-go"
 	"github.com/hashicorp/vault-client-go/schema"
+	"github.com/rs/zerolog"
 )
 
 type MultiClusterVaultClient struct {
@@ -84,10 +87,7 @@ func newClusterManager(cfg config.VaultConfig) (*clusterManager, error) {
 }
 
 func (cm *clusterManager) authenticate(ctx context.Context) error {
-	log.Logger.Info().Str("app_role", cm.config.AppRoleID).
-		Str("app_role_mount", cm.config.AppRoleMount).
-		Str("vault_address", cm.config.Address).
-		Msg("Authenticating with Vault using AppRole")
+	cm.decorateLog(log.Logger.Info, "authenticate").Msg("Authenticating with Vault")
 
 	res, err := cm.client.Auth.AppRoleLogin(
 		ctx,
@@ -98,10 +98,61 @@ func (cm *clusterManager) authenticate(ctx context.Context) error {
 		vault.WithMountPath(cm.config.AppRoleMount),
 	)
 	if err != nil {
+		cm.decorateLog(log.Logger.Error, "authenticate").Err(err)
 		return fmt.Errorf("failed to authenticate with role ID: %s at mount %s. (%w)", cm.config.AppRoleID, cm.config.AppRoleMount, err)
 	}
 	if err := cm.client.SetToken(res.Auth.ClientToken); err != nil {
+		cm.decorateLog(log.Logger.Error, "authenticate").Err(err)
 		return fmt.Errorf("failed to set client token: %w", err)
 	}
 	return nil
+}
+
+const (
+	fiveMinutes = 5 * time.Minute
+)
+
+func (cm *clusterManager) ensureValidToken(ctx context.Context) error {
+	reauthenticate := func(msg string, ttlSeconds int64, err error) error {
+		cm.decorateLog(log.Logger.Warn, "ensure_valid_token").
+			Int64("ttl_seconds", ttlSeconds).
+			Err(err).
+			Msg(msg)
+		return cm.authenticate(ctx)
+	}
+
+	cm.decorateLog(log.Logger.Debug, "ensure_valid_token").Msg("Ensuring Vault token is valid")
+
+	resp, err := cm.client.Auth.TokenLookUpSelf(ctx)
+	if err != nil {
+		return reauthenticate("Failed to look up token, re-authenticating", 0, err)
+	}
+
+	data := resp.Data
+	if ttlInterface, ok := data["ttl"]; ok {
+		ttlSeconds, err := converter.ConvertInterfaceToInt64(ttlInterface)
+		if err != nil {
+			return reauthenticate("Could not parse token TTL, re-authenticating", 0, err)
+		}
+
+		fiveMinutesInSeconds, _ := converter.ConvertInterfaceToInt64(fiveMinutes.Seconds())
+		if ttlSeconds < fiveMinutesInSeconds {
+			return reauthenticate("Token TTL is low, re-authenticating", ttlSeconds, nil)
+		}
+
+		cm.decorateLog(log.Logger.Debug, "ensure_valid_token").
+			Int64("ttl_seconds", ttlSeconds).
+			Msg("Token is valid")
+
+		return nil
+	}
+
+	return reauthenticate("Could not determine token TTL, re-authenticating", 0, nil)
+}
+
+func (cm *clusterManager) decorateLog(eventFactory func() *zerolog.Event, event string) *zerolog.Event {
+	return eventFactory().Str("app_role", cm.config.AppRoleID).
+		Str("app_role_mount", cm.config.AppRoleMount).
+		Str("vault_address", cm.config.Address).
+		Str("event", event)
 }
