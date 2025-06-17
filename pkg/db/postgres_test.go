6afd20641b8293935f1580319cb73a62f966b081
Mo <Binsabbar@users.noreply.github.com>
2025-06-17 16:33:09 +0300

    Mo: refactor psql code and test

--- pkg/db/postgres_test.go
+++ pkg/db/postgres_test.go
@@ -10,8 +10,6 @@ import (
 	"time"
 
 	"github.com/golang-migrate/migrate/v4/source"
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
 
 	"vault-sync/internal/config"
@@ -22,12 +20,6 @@ import (
 type PostgresDatastoreTestSuite struct {
 	suite.Suite
 	pgHelper *testutil.PostgresHelper
-	store    *PostgresDatastore
-}
-
-type testColumn struct {
-	DataType   string
-	IsNullable string
 }
 
 type badMigrationSource struct{}
@@ -39,6 +31,24 @@ func (b *badMigrationSource) GetSourceDriver() (source.Driver, error) {
 	return nil, fmt.Errorf("failed to create migration source: simulated error")
 }
 
+func (suite *PostgresDatastoreTestSuite) SetupTest() {
+	ctx := context.Background()
+	var err error
+
+	suite.pgHelper, err = testutil.NewPostgresContainer(suite.T(), ctx)
+	suite.NoError(err, "Failed to create Postgres test container")
+}
+
+func (suite *PostgresDatastoreTestSuite) TearDownTest() {
+	ctx := context.Background()
+	if suite.pgHelper != nil {
+		err := suite.pgHelper.Terminate(ctx)
+		if err != nil {
+			log.Printf("Error terminating container: %v", err)
+		}
+	}
+}
+
 func TestPostgresDatastoreSuite(t *testing.T) {
 	if os.Getenv("SKIP_INTEGRATION_TESTS") == "true" {
 		t.Skip("Skipping integration tests")
@@ -46,46 +56,21 @@ func TestPostgresDatastoreSuite(t *testing.T) {
 	suite.Run(t, new(PostgresDatastoreTestSuite))
 }
 
-func (s *PostgresDatastoreTestSuite) SetupSuite() {
-	ctx := context.Background()
-	var err error
-
-	s.pgHelper, err = testutil.NewPostgresContainer(s.T(), ctx)
-	require.NoError(s.T(), err, "Failed to create Postgres test container")
-}
-
-func (s *PostgresDatastoreTestSuite) TearDownSuite() {
-	ctx := context.Background()
-	if s.store != nil {
-		err := s.store.Close()
-		if err != nil {
-			log.Printf("Error closing datastore: %v", err)
-		}
-	}
-	if s.pgHelper != nil {
-		err := s.pgHelper.Terminate(ctx)
-		if err != nil {
-			log.Printf("Error terminating container: %v", err)
-		}
-	}
-}
-
 var postgresMigrator = migrations.NewPostgresMigration()
 
-func (s *PostgresDatastoreTestSuite) TestNewPostgresDatastore() {
+func (suite *PostgresDatastoreTestSuite) TestNewPostgresDatastore() {
 
-	s.T().Run("successful connection to postgres", func(t *testing.T) {
-		store, err := NewPostgresDatastore(*s.pgHelper.Config, postgresMigrator)
-		s.store = store
-		require.NoError(s.T(), err, "Should create datastore without error")
+	suite.Run("successful connection to postgres", func() {
+		store, err := NewPostgresDatastore(suite.pgHelper.Config, postgresMigrator)
+		suite.NoError(err, "Should create datastore without error")
 
-		assert.NotNil(s.T(), s.store, "Expected store to be non-nil on successful connection")
-		assert.NotNil(s.T(), s.store.DB, "Expected store.DB to be non-nil on successful connection")
-		assert.Equal(s.T(), "pgx", s.store.DB.DriverName(), "Expected driver name to be 'pgx'")
+		suite.NotNil(store, "Expected store to be non-nil on successful connection")
+		suite.NotNil(store.DB, "Expected store.DB to be non-nil on successful connection")
+		suite.Equal("pgx", store.DB.DriverName(), "Expected driver name to be 'pgx'")
 	})
 
-	s.T().Run("db connection failure returns error", func(t *testing.T) {
-		badConfig := config.Postgres{
+	suite.Run("db connection failure returns error", func() {
+		badConfig := &config.Postgres{
 			Address:  "localhost",
 			Port:     9999,
 			Username: "wrong",
@@ -95,133 +80,133 @@ func (s *PostgresDatastoreTestSuite) TestNewPostgresDatastore() {
 
 		store, err := NewPostgresDatastore(badConfig, postgresMigrator)
 
-		assert.Nil(s.T(), store, "Expected store to be nil on connection failure")
-		assert.Error(s.T(), err, "Expected error when connecting to invalid postgres instance")
-		assert.Contains(s.T(), err.Error(), "failed to connect to postgres", "Error message should indicate connection failure")
+		suite.Nil(store, "Expected store to be nil on connection failure")
+		suite.Error(err, "Expected error when connecting to invalid postgres instance")
+		suite.Contains(err.Error(), "failed to connect to postgres", "Error message should indicate connection failure")
 	})
 
-	s.T().Run("set maxConnection when it is configured", func(t *testing.T) {
-		cfg := *s.pgHelper.Config
+	suite.Run("set maxConnection when it is configured", func() {
+		cfg := testutil.CopyStruct(suite.pgHelper.Config)
 		cfg.MaxConnections = 5
 		store, err := NewPostgresDatastore(cfg, postgresMigrator)
-		s.store = store
-		require.NoError(s.T(), err, "Should create datastore without error")
+		suite.NoError(err, "Should create datastore without error")
+å
+		got := store.DB.Stats().MaxOpenConnections
 
-		got := s.store.DB.Stats().MaxOpenConnections
-
-		assert.Equal(s.T(), cfg.MaxConnections, got, "MaxOpenConnections should match config.MaxConnections")
+		suite.Equal(cfg.MaxConnections, got, "MaxOpenConnections should match config.MaxConnections")
 	})
 }
 
-func (s *PostgresDatastoreTestSuite) TestInitSchema_VerifySTableStructure() {
+type testColumn struct {
+	DataType   string
+	IsNullable string
+}
 
-	s.T().Run("verifies synced_secrets table structure", func(t *testing.T) {
+func (suite *PostgresDatastoreTestSuite) TestInitSchema_VerifySTableStructure() {
+
+	suite.Run("verifies synced_secrets table structure", func() {
 		expectedColumns := map[string]testColumn{
 			"secret_backend":      {"text", "NO"},
 			"secret_path":         {"text", "NO"},
 			"destination_cluster": {"text", "NO"},
 			"source_version":      {"integer", "NO"},
-			"destination_version": {"integer", "YES"},
+			"destination_version": {"integer", "NO"},
 			"last_sync_attempt":   {"timestamp with time zone", "NO"},
 			"last_sync_success":   {"timestamp with time zone", "YES"},
 			"status":              {"text", "NO"},
 			"error_message":       {"text", "YES"},
 		}
 
-		store, err := NewPostgresDatastore(*s.pgHelper.Config, postgresMigrator)
-		require.NoError(s.T(), err, "Should create datastore without error")
-		s.store = store
+		store, err := NewPostgresDatastore(suite.pgHelper.Config, postgresMigrator)
+		suite.NoError(err, "Should create datastore without error")
 
-		actualColumns := s.getColumns("public", "synced_secrets")
+		actualColumns := getColumns(store, "public", "synced_secrets")
 
-		assert.Len(s.T(), actualColumns, len(expectedColumns), "Number of columns does not match expected")
+		suite.Len(actualColumns, len(expectedColumns), "Number of columns does not match expected")
 
 		for col, exp := range expectedColumns {
 			act, ok := actualColumns[col]
-			assert.True(s.T(), ok, "Expected column '%s' not found", col)
-			assert.Equal(s.T(), exp.DataType, act.DataType, "Data type mismatch for column '%s'", col)
-			assert.True(s.T(), strings.EqualFold(exp.IsNullable, act.IsNullable), "Nullability mismatch for column '%s'", col)
+			suite.True(ok, "Expected column '%s' not found", col)
+			suite.Equal(exp.DataType, act.DataType, "Data type mismatch for column '%s'", col)
+			suite.True(strings.EqualFold(exp.IsNullable, act.IsNullable), "Nullability mismatch for column '%s'", col)
 		}
 	})
 
-	s.T().Run("verifies primary key constraint on id column", func(t *testing.T) {
-		store, err := NewPostgresDatastore(*s.pgHelper.Config, postgresMigrator)
-		require.NoError(s.T(), err, "Should create datastore without error")
-		s.store = store
+	suite.Run("verifies primary key constraint on id column", func() {
+		store, err := NewPostgresDatastore(suite.pgHelper.Config, postgresMigrator)
+		suite.NoError(err, "Should create datastore without error")
 
-		var pkColumns = s.getPrimaryKeyColumns("public", "synced_secrets")
+		var pkColumns = getPrimaryKeyColumns(store, "public", "synced_secrets")
 
-		assert.Equal(s.T(), []string{"secret_backend", "secret_path", "destination_cluster"}, pkColumns, "PRIMARY KEY should be on 'id'")
+		suite.Equal([]string{"secret_backend", "secret_path", "destination_cluster"}, pkColumns, "PRIMARY KEY should be on 'id'")
 	})
 
-	s.T().Run("returns error if migration source is broken", func(t *testing.T) {
+	suite.Run("returns error if migration source is broken", func() {
 		// Custom migration source that always fails
 		badSource := &badMigrationSource{}
-		_, err := NewPostgresDatastore(*s.pgHelper.Config, badSource)
-		assert.Error(t, err)
-		assert.Contains(t, err.Error(), "failed to create migration source")
+
+		_, err := NewPostgresDatastore(suite.pgHelper.Config, badSource)
+
+		suite.Error(err)
+		suite.Contains(err.Error(), "failed to create migration source")
 	})
 
-	s.T().Run("returns error if migration fails", func(t *testing.T) {
-		store, err := NewPostgresDatastore(*s.pgHelper.Config, migrations.NewPostgresMigration())
-		require.NoError(t, err)
-		s.store = store
+	suite.Run("returns error if migration fails", func() {
+		store, err := NewPostgresDatastore(suite.pgHelper.Config, migrations.NewPostgresMigration())
+		suite.NoError(err)
 
-		s.pgHelper.Stop(context.Background(), nil)
-		defer s.pgHelper.Start(context.Background())
+		suite.pgHelper.Stop(context.Background(), nil)
+		defer suite.pgHelper.Start(context.Background())
 
-		err = s.store.initSchema()
-		assert.Error(t, err)
+		err = store.initSchema()
+		suite.Error(err)
 	})
 
 }
 
-func (s *PostgresDatastoreTestSuite) TestHealthCheck() {
-	s.T().Run("health check continues after database temporary outage", func(t *testing.T) {
+func (suite *PostgresDatastoreTestSuite) TestHealthCheck() {
+	suite.Run("health check continues after database temporary outage", func() {
 		shortInterval := 300 * time.Millisecond
 		originalHealthCheckPeriod := defaultHealthCheckPeriod
 		defaultHealthCheckPeriod = shortInterval
 		defer func() { defaultHealthCheckPeriod = originalHealthCheckPeriod }()
 
-		config := *s.pgHelper.Config
-		store, err := NewPostgresDatastore(config, postgresMigrator)
-		require.NoError(t, err)
-		s.store = store
+		store, err := NewPostgresDatastore(suite.pgHelper.Config, postgresMigrator)
+		suite.NoError(err)
 
 		// Let it run a few cycles
 		time.Sleep(shortInterval * 3)
 
 		ctx := context.Background()
-		err = s.pgHelper.Stop(ctx, &shortInterval)
+		err = suite.pgHelper.Stop(ctx, &shortInterval)
 
 		// Wait for a few health check cycles during the outage
 		time.Sleep(shortInterval * 3)
 
 		// Resume the container
-		err = s.pgHelper.Start(ctx)
-		require.NoError(t, err)
+		err = suite.pgHelper.Start(ctx)
+		suite.NoError(err)
 
-		assert.Eventually(t, func() bool {
+		suite.Eventually(func() bool {
 			var count int
 			err = store.DB.Get(&count, "SELECT 1")
 			return err == nil && count == 1
 		}, time.Second*10, time.Millisecond*100, "Health check should still be running after recovery")
 	})
 
-	s.T().Run("it stops healthcheck when DB is closed", func(t *testing.T) {
-		config := *s.pgHelper.Config
-		store, err := NewPostgresDatastore(config, postgresMigrator)
-		s.store = store
-		require.NoError(t, err, "Should create datastore without error")
+	suite.Run("it stops healthcheck when DB is closed", func() {
+		store, err := NewPostgresDatastore(suite.pgHelper.Config, postgresMigrator)
 
-		s.store.Close()
+		suite.NoError(err, "Should create datastore without error")
 
-		assert.Nil(t, s.store.healthCheckInterval)
-		assert.Nil(t, s.store.stopHealthCheckCh)
+		store.Close()
+
+		suite.Nil(store.healthCheckInterval)
+		suite.Nil(store.stopHealthCheckCh)
 	})
 }
 
-func (s *PostgresDatastoreTestSuite) getColumns(schema string, table string) map[string]testColumn {
+func getColumns(store *PostgresDatastore, schema string, table string) map[string]testColumn {
 	query := `
 				SELECT column_name, data_type, is_nullable
 				FROM information_schema.columns
@@ -229,19 +214,19 @@ func (s *PostgresDatastoreTestSuite) getColumns(schema string, table string) map
 					AND table_name   = $2
 				ORDER BY ordinal_position;
 		`
-	rows, _ := s.store.DB.Queryx(query, schema, table)
+	rows, _ := store.DB.Queryx(query, schema, table)
 	defer rows.Close()
 
 	cols := make(map[string]testColumn)
 	for rows.Next() {
 		var name, dataType, isNullable string
-		assert.NoError(s.T(), rows.Scan(&name, &dataType, &isNullable))
+		rows.Scan(&name, &dataType, &isNullable)
 		cols[name] = testColumn{dataType, isNullable}
 	}
 	return cols
 }
 
-func (s *PostgresDatastoreTestSuite) getConstraintColumns(constraintName string, schema string, table string) []string {
+func getConstraintColumns(store *PostgresDatastore, constraintName string, schema string, table string) []string {
 	query := `
         SELECT kcu.column_name
         FROM information_schema.table_constraints tc
@@ -254,11 +239,11 @@ func (s *PostgresDatastoreTestSuite) getConstraintColumns(constraintName string,
         ORDER BY kcu.ordinal_position;
     `
 	var columns []string
-	s.store.DB.Select(&columns, query, constraintName, schema, table)
+	store.DB.Select(&columns, query, constraintName, schema, table)
 	return columns
 }
 
-func (s *PostgresDatastoreTestSuite) getPrimaryKeyColumns(schema string, table string) []string {
+func getPrimaryKeyColumns(store *PostgresDatastore, schema string, table string) []string {
 	query := `
         SELECT kcu.column_name
         FROM information_schema.table_constraints tc
@@ -271,6 +256,6 @@ func (s *PostgresDatastoreTestSuite) getPrimaryKeyColumns(schema string, table s
         ORDER BY kcu.ordinal_position;
     `
 	var columns []string
-	s.store.DB.Select(&columns, query, schema, table)
+	store.DB.Select(&columns, query, schema, table)
 	return columns
 }
