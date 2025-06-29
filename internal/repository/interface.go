781ae85d01fe51216deb69d9aafd40c3f8f33ebf
Mo <Binsabbar@users.noreply.github.com>
2025-06-29 16:42:44 +0300

    Mo: refactor repo and use explicit type

--- internal/repository/interface.go
+++ internal/repository/interface.go
@@ -4,7 +4,7 @@ import "vault-sync/internal/models"
 
 type SyncedSecretRepository interface {
 	GetSyncedSecret(backend, path, destinationCluster string) (*models.SyncedSecret, error)
-	UpdateSyncedSecretStatus(secret models.SyncedSecret) error
-	GetSyncedSecrets() ([]models.SyncedSecret, error)
+	UpdateSyncedSecretStatus(secret *models.SyncedSecret) error
+	GetSyncedSecrets() ([]*models.SyncedSecret, error)
 	Close() error
 }
