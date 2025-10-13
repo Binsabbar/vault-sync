4f75ed944c1aea17f473eff636ed2c29a2b992d3
Mohammed Binsabbar <ibinsabbar@gmail.com>
2025-10-13 15:30:13 +0300

    Mo: implement sync command

--- cmd/sync/sync.go
+++ cmd/sync/sync.go
@@ -2,6 +2,8 @@ package sync
 
 import (
 	"fmt"
+	"vault-sync/internal/config"
+	"vault-sync/internal/core"
 
 	"github.com/spf13/cobra"
 )
@@ -15,5 +17,20 @@ var SyncCmd = &cobra.Command{
 }
 
 func run(cmd *cobra.Command, args []string) {
-	fmt.Println("sync called")
+
+	appConfig, err := config.NewConfig()
+	if err != nil {
+		fmt.Printf("Error creating config: %v\n", err)
+		return
+	}
+
+	wiring := core.NewWiring(appConfig)
+	ctx := cmd.Context()
+	orchestrator := wiring.InitOrchestrator(ctx)
+	_, err = orchestrator.StartSync(ctx)
+	if err != nil {
+		fmt.Printf("Error during sync: %v\n", err)
+		return
+	}
+	fmt.Println("Sync process completed successfully")
 }
