8b417f256df379a660e3019137594aee37815aef
Mo <Binsabbar@users.noreply.github.com>
2025-10-15 15:57:05 +0300

    Mo: add version subcommand

--- main.go
+++ main.go
@@ -2,6 +2,15 @@ package main
 
 import "vault-sync/cmd"
 
+var (
+	version = "dev"
+	commit  = "none"
+	date    = "unknown"
+	builtBy = "unknown"
+)
+
 func main() {
+	// Set version info before executing commands
+	cmd.SetVersionInfo(version, commit, date, builtBy)
 	cmd.Execute()
 }
